e4a4f36d782f125004dd4211aec8c44a344ab023
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-05-24 23:02:45 -0300

    Fixed some imports

--- lib/features/number_trivia/domain/repositories/i_number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/i_number_trivia_repository.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
-import 'package:trivia_clean_arch_tdd/core/errors/failures/i_failure.dart';
-import 'package:trivia_clean_arch_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import '../../../../core/errors/failures/i_failure.dart';
+import '../entities/number_trivia.dart';
 
 abstract class INumberTriviaRepository {
   Future<Either<IFailure, NumberTrivia>> getConcreteNumberTrivia(
