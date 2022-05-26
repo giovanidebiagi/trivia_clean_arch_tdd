51d627408bded42a72da504362ab85aa213cd7b3
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-05-26 09:10:52 -0300

    Created injection_container for dependency injection using GetIt as ServiceLocator.

---
+++ lib/core/injection_container.dart
@@ -0,0 +1,83 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:trivia_clean_arch_tdd/features/number_trivia/data/datasources/i_number_trivia_local_datasource.dart';
+import 'package:trivia_clean_arch_tdd/features/number_trivia/data/datasources/shared_preferences_number_trivia_local_datasource.dart';
+import '../features/number_trivia/data/datasources/dio_number_trivia_remote_datasource.dart';
+import '../features/number_trivia/data/datasources/i_number_trivia_remote_datasource.dart';
+import '../features/number_trivia/data/repositories/number_trivia_repository.dart';
+import '../features/number_trivia/domain/entities/number_trivia.dart';
+import '../features/number_trivia/domain/repositories/i_number_trivia_repository.dart';
+import '../features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import '../features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import '../features/number_trivia/presentation/blocs/number_trivia_bloc.dart';
+import 'network/i_network_service.dart';
+import 'network/network_service.dart';
+import 'usecases/i_usecase.dart';
+import 'utils/type_converting_utils.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> init() async {
+  //! Features
+  // Datasources
+  getIt.registerLazySingleton<INumberTriviaLocalDatasource>(
+    () => SharedPreferencesNumberTriviaLocalDatasource(
+      sharedPreferences: getIt(),
+    ),
+  );
+  getIt.registerLazySingleton<INumberTriviaRemoteDatasource>(
+    () => DioNumberTriviaRemoteDatasource(
+      dio: getIt(),
+    ),
+  );
+
+  // Repositories
+  getIt.registerLazySingleton<INumberTriviaRepository>(
+    () => NumberTriviaRepository(
+      networkService: getIt(),
+      numberTriviaLocalDatasource: getIt(),
+      numberTriviaRemoteDatasource: getIt(),
+    ),
+  );
+
+  // Usecases
+  getIt.registerLazySingleton<IUseCase<NumberTrivia, Params>>(
+    () => GetConcreteNumberTrivia(
+      repository: getIt(),
+    ),
+  );
+  getIt.registerLazySingleton<IUseCase<NumberTrivia, NoParams>>(
+    () => GetRandomNumberTrivia(
+      repository: getIt(),
+    ),
+  );
+
+  // BLoCs
+  getIt.registerFactory(
+    () => NumberTriviaBloc(
+      getConcreteNumberTrivia: getIt(),
+      typeConvertingUtils: getIt(),
+      getRandomNumberTrivia: getIt(),
+    ),
+  );
+
+  //! External
+  final sharedPreferencess = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferencess);
+
+  getIt.registerLazySingleton<InternetConnectionChecker>(
+      () => InternetConnectionChecker());
+  getIt.registerLazySingleton<Dio>(
+    () => Dio(),
+  );
+
+  //! Core
+  getIt.registerLazySingleton<INetworkService>(
+    () => NetworkService(
+      internetConnectionChecker: getIt(),
+    ),
+  );
+  getIt.registerLazySingleton<TypeConvertingUtils>(() => TypeConvertingUtils());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:trivia_clean_arch_tdd/core/injection_container.dart'
+    as injection_container;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await injection_container.init();
+
   runApp(const MyApp());
 }
 
