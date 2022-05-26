e592bdca1a1ae6677dcaf5bcdb19e0ae24bb9b53
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-05-26 08:58:09 -0300

    Created failure_message_constants.dart file

---
+++ lib/core/errors/failures/failure_message_constants.dart
@@ -0,0 +1,4 @@
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
