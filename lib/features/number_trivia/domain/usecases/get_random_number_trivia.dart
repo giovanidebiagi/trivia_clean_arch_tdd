29976b5407ba42876e4b4dda0e13a563965bfee4
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-05-26 08:55:47 -0300

    Fixed duplicated NoParams class.

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import 'package:trivia_clean_arch_tdd/core/errors/failures/i_failure.dart';
 import 'package:trivia_clean_arch_tdd/core/usecases/i_usecase.dart';
 import 'package:trivia_clean_arch_tdd/features/number_trivia/domain/entities/number_trivia.dart';
@@ -15,9 +14,4 @@ class GetRandomNumberTrivia implements IUseCase<NumberTrivia, NoParams> {
       {required NoParams params}) async {
     return await repository.getRandomNumberTrivia();
   }
-}
-
-class NoParams extends Equatable {
-  @override
-  List<Object?> get props => [];
 }
--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:trivia_clean_arch_tdd/core/usecases/i_usecase.dart';
 import 'package:trivia_clean_arch_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:trivia_clean_arch_tdd/features/number_trivia/domain/repositories/i_number_trivia_repository.dart';
 import 'package:trivia_clean_arch_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
